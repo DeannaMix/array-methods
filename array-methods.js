b691c8c4e30f7f54af954821029cc342bba46b9e
Deanna Mix <deanna@LAPTOP-PG68556H.localdomain>
2020-08-21 21:43:31 -0500

    exercise done

--- array-methods.js
+++ array-methods.js
@@ -24,8 +24,13 @@ planets.forEach(gasball => {
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 
-const upperGasballs = planets.map(gasball => {
-    return gasball.toUpperCase()
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const upperGasballs = planets.map(gasball => {
+    return capitalizeFirstLetter(gasball)
 })
 console.log(upperGasballs)
 
@@ -37,3 +42,13 @@ console.log(upperGasballs)
 
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
 */
+
+const gasballsWithE = planets.filter(gasball => {
+    if (gasball.includes('e')) {
+        return true
+    } else {
+        return false
+    }
+})
+
+console.log(gasballsWithE)
